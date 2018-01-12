88ccc13f3d24be075b6ee869d535105bd0ee73c3
mcoluzzi <mcoluzzi@visitops.com>
2018-01-11 22:05:53 -0500

    Updated Three -- While Condition

--- Coluzzi_ExerciseThree.js
+++ Coluzzi_ExerciseThree.js
@@ -2,7 +2,7 @@ var userInput = prompt("Would you like to print your name?")
 var userName = prompt("What is your name?")
 var nameString = "Hello. My name is " + userName;
 
-while (userInput != "No") {
+while (userInput == "Yes") {
   console.log(nameString)
   nameString = nameString + "!"
   userInput = prompt("Would you like to print this again?")
